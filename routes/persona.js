a137964ac2b012e50a47f64f6056aea59ab38ac5
why be <whybe7@hotmail.com>
2023-04-21 21:24:20 +0000

    persona endpoint 🏳️

--- routes/persona.js
+++ routes/persona.js
@@ -1,10 +1,24 @@
+const Persona = require('../models/Persona');
+const { isSupportedCountry } = require('../utils');
+
 // load .env file
 require('dotenv').config();
 
 const personaRoute = (req, res)=>{
-    // const { gender }
+    let { gender, country, amount } = req.body;
     try{
-
+        // check for required fields
+        if(!gender) return res.status(400).send(null);
+        // check for supported countries
+        // if(country && !isSupportedCountry(country)) return res.status(400).send(null);
+        // limit the amount of profiles to 100 at most
+        amount = Math.abs(amount) > 100 ? 100 : Math.abs(amount);
+        // generate profile Persona based on the amount
+        const profiles = [];
+        for(let i = 0; i < amount; i++){
+            profiles.push(new Persona(gender));
+        }
+        res.status(200).json(profiles);
     }
     catch(error){
         console.log(error);
