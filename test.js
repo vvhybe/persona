72815548d55b5716b920812f91bbb78ba0e87596
whybe7 <whybe7@hotmail.com>
2023-04-23 02:23:20 +0200

    test 🧪 file to test commandsm properties, methods

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log(require("faker").phone.phoneFormats());
+// using fake zip codes
+// console.log(require("faker").address.zipCode());
+// using fake street names
+// console.log(require("faker").address.streetName());
+// generate fake names based on the gender
+console.log(require("faker").name.firstName());
+console.log(require("faker").name.gender());
